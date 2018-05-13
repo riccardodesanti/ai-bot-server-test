2e8cd2444a65105a05c102b88b7ec52bf5aeb0c4
riccardodesanti <riccardodesantimail@gmail.com>
2018-05-13 11:41:42 +0200

    debugged

--- index.js
+++ index.js
@@ -133,7 +133,7 @@ function handlePostback(sender_psid, received_postback) {
   if ( payload === 'yes') {
     response = { "text": "Thanks!" }
   } else if ( payload === 'no') {
-    respnse = { "text": "Oops, try sending another image."}
+    response = { "text": "Oops, try sending another image."}
   }
 
   // Send the message to acknowledge the postback
